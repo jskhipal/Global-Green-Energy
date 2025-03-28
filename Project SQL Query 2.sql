3d7ab104f506dd4ea98ac230dbe3392397d5da76
Jaspal Singh <jaspalkhipal20@gmail.com>
2025-03-28 11:37:23 +0530

    Add files via upload

---
+++ Project SQL Query 2.sql
@@ -0,0 +1,195 @@
+select * from GG_Energy
+
+--List all unique energy sources available in the dataset.
+
+select EnergySource
+from GG_Energy
+Group by EnergySource
+
+--Count the total number of records for each country.
+
+select country, Count(Country) as Total_records
+from GG_Energy
+group by Country
+
+--Find the average energy production for each energy source.
+
+select EnergySource, avg(cast(EnergyProduction as float)) as Avg_engery_produced
+from GG_Energy
+group by EnergySource
+
+--Calculate the total carbon reduction for each year.
+
+select year, Sum(cast(CarbonProduction as float)) as Total_carbon_production
+from GG_Energy
+group by year
+order by Year desc
+
+--Retrieve the top 5 countries with the highest energy production.
+
+select top 5 country, sum(cast(EnergyProduction as float)) as Total_energy_Production
+from GG_Energy
+group by Country
+order by Total_energy_Production desc
+
+--Find the country with the lowest carbon reduction in a given year.
+
+select top 1 country, sum(cast(EnergyProduction as float)) as Total_energy_Production
+from GG_Energy
+group by Country
+Order by Total_energy_Production ASC
+
+--Group investments by energy source and calculate the average investment.
+
+select EnergySource, avg(cast(Investment as float)) as Avg_Investment
+from GG_Energy
+Group by EnergySource
+
+--List the top 10 records based on jobs created.
+
+select top 10 * 
+from GG_Energy
+order by JobsCreated desc
+
+--Find countries with more than 100000 jobs created.
+
+select Country, JobsCreated
+from GG_Energy
+where JobsCreated > 100000
+
+--Calculate the total investment made by each country.
+
+select Country, Sum(cast(Investment as Float)) As Total_investment
+from GG_Energy
+Group by Country
+
+--Find the highest energy production in each year.
+
+select Year, max(EnergyProduction) As Highest_energy_production
+from GG_Energy
+Group by Year
+
+--Retrieve records where carbon reduction is above 500000.
+
+select *
+from GG_Energy
+where CarbonProduction > 500000
+
+--Find the average carbon Production for each energy source.
+
+select EnergySource, Avg(cast(CarbonProduction as float)) as Avg_Carbon_Production
+from GG_Energy
+group by EnergySource
+
+--List countries that invested more than $500 million.
+
+select Country, sum(cast(Investment as float)) as Total_Investment
+From GG_Energy
+where Investment > 500000000
+group by Country 
+
+--Calculate the total energy production per year.
+
+select Year, Sum(Cast(EnergyProduction as float)) as Total_Energy_production
+from GG_Energy
+Group by year
+
+--Find countries that produced more than 50000 units of solar energy.
+
+select Country, sum(cast(EnergyProduction as float)) as Total_energy_production
+from GG_Energy
+where EnergySource = 'Solar' 
+group by Country
+having sum(cast(EnergyProduction as float)) > 50000
+ 
+-- Count the number of records where investment exceeds $1 billion.
+
+select *
+from GG_Energy
+where Investment > 1000000000
+
+--Find the average number of jobs created per energy source.
+
+select EnergySource, Avg(cast(JobsCreated as float)) as Avg_Job_Created
+from GG_Energy
+group by EnergySource
+
+--List countries with the maximum and minimum energy production.
+
+select top 1 Country, Max(EnergyProduction) as Max_EProduction 
+from GG_Energy
+Group BY Country
+order by  Max(EnergyProduction) desc 
+
+select top 1 Country, Min(EnergyProduction) as MIn_Eproduction
+from GG_Energy
+Group BY Country
+order by  Min(EnergyProduction) asc
+
+--Retrieve records sorted by carbon Production in descending order.
+
+select *
+from GG_Energy
+order by CarbonProduction desc
+
+--Calculate the yearly average jobs created.
+
+select year, Avg(cast(JobsCreated as float)) as Avg_Jobs_created
+from GG_Energy
+Group by Year
+
+--Find the most common energy source for each country.
+
+select country, 
+mode() within group (Order by EnergySource) as MostCommonEnergy
+from GG_Energy
+group by Country
+
+--Retrieve the total carbon production by Country
+
+select Country, Sum(cast(CarbonProduction as Float)) as Total_Carbon_Production
+from GG_Energy
+Group by Country
+
+--Calculate the difference between maximum and minimum energy production for each country.
+
+select Country, max(EnergyProduction)-min(EnergyProduction) as Production_Difference
+from GG_Energy
+group by Country
+
+--Identify energy sources with average carbon production above 300000.
+
+Select EnergySource, Avg(cast(carbonProduction as float)) as Avg_Carbon_Production
+from GG_Energy
+group by EnergySource
+having Avg(cast(carbonProduction as float)) > 300000
+
+--Find records where jobs created are below the average for that energy source.
+
+select *
+from GG_Energy as g1
+where JobsCreated < (
+						select  Avg(cast(JobsCreated as float)) as AvgjobCreated
+						from GG_Energy as g2
+						where g1.EnergySource = g2.EnergySource
+						)
+
+
+
+--List records where the investment per job created is the highest
+
+select *
+from GG_Energy
+where (investment /nullif(JobsCreated, 0)) = ( select max(investment / nullif(jobscreated, 0))
+												from GG_Energy)
+
+--Retrieve the first and last records for each energy source.
+
+with rank as ( select *,
+						rank () over (partition by EnergySource order by year desc) as firstrecord,
+						Rank () over (partition by EnergySource order by year asc) as lastrecord
+						from GG_Energy
+						) 
+						  select *
+						  from rank
+						  where firstrecord = 1 or lastrecord = 1;
---
+++ Project SQL Query.sql
@@ -0,0 +1,219 @@
+use jaspaldb
+
+select * from dbo.GGEnergy
+
+create table GG_Energy(
+Country varchar (20),
+year int,
+EnergySource varchar(20),
+EnergyProduction int,
+CarbonProduction int,
+Investment int,
+JobsCreated int
+);
+
+insert into GG_Energy(
+Country,
+Year,
+EnergySource,
+EnergyProduction,
+CarbonProduction,
+Investment,
+JobsCreated
+)
+
+select Country,
+year,
+EnergySource,
+EnergyProduction,
+CarbonReduction,
+Investment,
+JobsCreated
+from dbo.GGEnergy
+
+select * 
+from GG_Energy
+
+--growth rate of energy production over the years.
+
+with yearlyproduction as(
+						select year, sum(cast(EnergyProduction as float)) as TotalProduction
+						from GG_Energy
+						Group BY year
+						),
+Growthrate as (
+				select year, TotalProduction,
+				lag(TotalProduction) over (order by year) as PreviousProduction,
+				case
+					when lag(TotalProduction) over (order by year) = 0 Then Null
+					Else ((TotalProduction - lag(TotalProduction) over (order by year)) / lag(TotalProduction) over (order by year)) * 100
+					end as Growthrate
+					from yearlyproduction
+					)
+select year, TotalProduction, Growthrate
+from Growthrate
+
+order by Growthrate desc
+
+--countries with the highest increase in investment from one year to the next.
+
+with investmentgrowth as (
+							select country, year, investment,
+							lag(Investment) over (partition by country order by year) as previuos_year_investment,
+							(investment - (lag(Investment) over (partition by country order by year))) as investment_increase
+							from GG_Energy
+											)
+											 select country, year, investment_increase
+											 from investmentgrowth
+											 where investment_increase = (
+																		  select max(investment_increase)
+																		  from investmentgrowth);
+
+--top 3 countries with the highest total energy production.
+
+select top 3 Country, sum(cast(EnergyProduction as float)) as total_energy_production
+from GG_Energy
+group by Country
+order by sum(cast(EnergyProduction as float)) desc
+
+--the percentage share of each energy source in total energy production.
+
+select EnergySource,
+sum(cast(EnergyProduction as float)) as Total_Energy_Production,
+((sum(cast(EnergyProduction as float)) *100) / (select Sum(cast(EnergyProduction as float)))) as percentage_share 
+from GG_Energy
+group by EnergySource
+order by percentage_share desc
+
+--correlation between investment and energy production
+
+-- investment likely doesn’t directly impact energy production as corelation is likely 0
+
+with corelation as(
+					select 
+					count(*) as N,																		--Total Number of rows, (N)
+					sum(cast(Investment as float)) as x,												--Sum of all Investments (x)
+					sum(cast(EnergyProduction as float)) as y,											--Sum of all energy production (y)					
+					sum((cast(Investment as float)) * cast(EnergyProduction as float)) as xy,			--sum of product of investment and energy production (xy)
+					sum((cast(Investment as float)) * cast(Investment as float)) as xx,					--sum of square of investment (xx)
+					sum((cast(EnergyProduction as float)) * cast(EnergyProduction as float)) as yy		--sum of square of energy production (xy)
+					from GG_Energy
+					)
+select (N * xy - x * y) /
+   (SQRT((N * xx - x * x) * (N * xx - y * y))) as stats
+   from corelation
+ 
+--cumulative investment for each country over the years.
+
+ select Country, year, Investment,
+ sum(cast(Investment as float)) over ( partition by Country 
+										order by year
+										rows between unbounded preceding and current row) as cumulative_investment 
+from GG_Energy
+order by Country, year
+
+--year-over-year percentage change in investment for each country.
+
+select Country , 
+	   Investment, 
+	   year,
+			lag(Investment) over (partition by Investment order by year) as previous_year_investment,
+						case 
+						 WHEN LAG(Investment) OVER (PARTITION BY country ORDER BY year) IS NOT NULL 
+							THEN ROUND( (CAST(Investment AS DECIMAL(18,2)) - CAST(LAG(Investment) OVER (PARTITION BY country ORDER BY year) AS DECIMAL(18,2))) 
+									/ CAST(LAG(Investment) OVER (PARTITION BY country ORDER BY year) AS DECIMAL(18,2)) * 100,2)
+							else null
+							end as YoY_investment_change
+from GG_Energy
+order by Country, Year
+
+--Ranking countries based on total investment in Renewable energy sources.
+
+select Country,
+sum(cast (Investment as float)) as total_investment,
+rank() over (order by (sum(cast (Investment as float))) desc) as RANKING
+from GG_Energy
+group by Country
+order by RANKING
+
+--the top 3 years with the highest investment for each country.
+
+with RankedInvestment as (
+select Country,
+year,
+SUM(cast(Investment as float)) as total_investment,
+rank() over (partition by Country order by SUM(cast(Investment as float)) asc) as Ranking
+from GG_Energy
+group by Country, year
+)
+  select * 
+  from RankedInvestment
+  where Ranking <=3
+  order by Country, Ranking
+
+--the first and last year of recorded investment for each country.
+
+select Country,
+max(year) as First_year_of_transaction,
+min(year) as Last_year_of_transaction
+from GG_Energy
+group by Country
+order by Country
+
+-- Detect investment trends (e.g., increasing or decreasing) for each country
+
+with investment_record as (
+						select Country, year,
+							sum(cast(Investment as float)) as total_investment,
+							lag(sum(cast(Investment as float))) over (partition by Country order by Year) as previous_year_investment,
+							((sum(cast(Investment as float))) - (lag(sum(cast(Investment as float))) over (partition by Country order by Year))) as change_in_investment
+						from GG_Energy
+						group by Country, year
+						)
+select Country, 
+	   year,
+	   total_investment,
+	   previous_year_investment,
+	   change_in_investment,
+	   case 
+			when previous_year_investment is null then 'N/A (first investent)'
+			when total_investment > previous_year_investment then 'Increasing'
+			when total_investment < previous_year_investment then 'Decreasing'
+			else 'NO Change'
+			end
+from investment_record
+order by Country, year;
+
+--Find the Energy Source with the Highest Variance in Investment Across Countries
+
+with investmentstats as
+(
+   select EnergySource,
+	 avg(cast(Investment as float)) as Total_investment,
+	   count(*) as Total_entries,
+		 (sum(cast(Investment as float)) / (count(*))) as mean_investment
+		   from GG_Energy
+		     group by EnergySource
+),
+   variance as 
+(
+   select g.EnergySource,
+	SUM(POWER(CAST(g.Investment AS FLOAT) - i.mean_investment, 2)) / COUNT(*) / count(*) as Variance_Investment
+	  from GG_Energy g
+		join investmentstats i
+		  on g.EnergySource = i.EnergySource
+		    group by g.EnergySource
+)
+   select EnergySource, Variance_Investment
+	 from variance
+	   order by Variance_Investment desc
+
+-- Optimize a query to quickly retrieve the top 10 countries by total investment.
+
+ --CREATE INDEX idx_country_investment ON GG_Energy (Country, Investment);
+SELECT TOP 10 
+Country, 
+SUM(cast(Investment as float)) AS total_investment
+FROM GG_Energy
+GROUP BY Country
+ORDER BY total_investment DESC; -- to check the speed of the execution(ctrl + M)
